7fdb89484c626124ec0990c552f4087eb89e12ae
Larry Skye <67720983+larryskye@users.noreply.github.com>
2021-09-15 22:43:18 +0100

    Create debian-buster-proxmox.sh

---
+++ raw/debian-buster-proxmox.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -o errexit  # Exit on most errors (see the manual)
+set -o errtrace # Make sure any error trap is inherited
+set -o nounset  # Disallow expansion of unset variables
+set -o pipefail # Use last non-zero exit code in a pipeline
+
+echo "deb [arch=amd64] http://download.proxmox.com/debian/pve buster pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
+
+wget http://download.proxmox.com/debian/proxmox-ve-release-6.x.gpg -O /etc/apt/trusted.gpg.d/proxmox-ve-release-6.x.gpg
+chmod +r /etc/apt/trusted.gpg.d/proxmox-ve-release-6.x.gpg
+
+apt update && apt full-upgrade
+
+apt install proxmox-ve postfix open-iscsi
